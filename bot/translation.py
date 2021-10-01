0d4c2e3846e033269b897bd5171fbd60c3270cbf
godgroot <85842733+godgroot@users.noreply.github.com>
2021-10-01 19:51:28 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,18 +6,18 @@ class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
 
-<b>I am a Pro AutoFilterBot V2....😜</b>
+<b>I am ashaan ashaan creation....😜</b>
 
-<b>Make me an admin for your group and channel then connect me....🎉</b>
+<b>You cant add me to your group prr....🎉</b>
 
-<b>🙂Watch the video below to build an Pro Autofilter bot V2 like me...</b>
+<b>🙂Ask movies On our group please...</b>
 
-<b>Subscribe to the update channel to learn about my updates and activity...</b>
+<b>share our Group please 😌😍...</b>
 
 <b>Press /help to know about available commands🤪</b>"""    
     
     HELP_TEXT = """
-<b>😉How To Use Me!😉?</b>
+<b>😉Poda Patti!😉?</b>
 
 <i>
 -> Add Me To Any Group And Make Me Admin
@@ -46,18 +46,18 @@ class Translation(object):
             
             -> <code>Status</code> - <b>Button Will Shows The Stats Of Your Channel</b>
             
-<b><a href="https://t.me/Mo_Tech_YT">© Mo Tech YT</a></b>
+<b><a href="https://t.me/CINEMAZIPLINKS/7">© Cinemazip</a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Cinemazip Movie Bot</b>
 
-<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/AlbertEinstein_TG">👤Albert Einstein</a></b>
+<b>>➥👨‍💼Master</b> : <b><a href="https://t.me/CINEMAZIPLINKS/7">👤Ashaan ashaan</a></b>
 
-<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/MRk_YT">👤Mrk YT</a></b>
+<b>➥ 👨‍💻Editor</b> : <b><a href="https://t.me/CINEMAZIPLINKS/7">👤The God</a></b>
 
 <b>➥ 🗣️Language</b> : <b>Python3<b>
 
 <b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MoTechYT/Pro-Auto-Filter-Bot-V2">💥Click Me</a></b>
+<b>➥ 📖Source Code</b> : <b><a href="https://t.me/CINEMAZIPLINKS/7">💥Click Me</a></b>
 """
